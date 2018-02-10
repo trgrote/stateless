c5db9d4b998a06bada7fde51ceeb70b9f7578104
Taylor Grote <grotese@gmail.com>
2018-02-09 21:02:10 -0600

    Add tuple class

---
+++ src/Stateless/Tuple.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stateless
+{
+    /// <summary>
+    /// Hopefully Nothing else is called Tuple
+    /// </summary>
+    /// <typeparam name="TAgo0"></typeparam>
+    /// <typeparam name="TArg1"></typeparam>
+    public class Tuple<TAgo0, TArg1>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public TAgo0 Item1 { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TArg1 Item2 { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item1"></param>
+        /// <param name="item2"></param>
+        public Tuple(TAgo0 item1, TArg1 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+}
